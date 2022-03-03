f0a0102f82909a27e164cf926459fe7fa42368bd
henrique2m <btdavintehenrique@gmail.com>
2022-03-02 22:20:58 -0300

    :sparkles: button component  :rocket:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,13 @@
-import  React from 'react';
+import React from "react";
 
-import { TextInputProps } from 'react-native';
+import { TextInputProps } from "react-native";
 
-import { Container, TypesProps} from './style';
+import { Container, TypesProps } from "./styles";
 
 type Props = TextInputProps & {
   type?: TypesProps;
-}
+};
 
-export function Input({ type = 'primary', ...rest}: Props) {
-  return <Container type={type} {...rest} />
-
+export function Input({ type = "primary", ...rest }: Props) {
+  return <Container type={type} {...rest} />;
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 import { Container } from "./styles";
 
 export function SignIn() {
-
-  return(
+  return (
     <Container>
       <Input
         placeholder="E-mail"
         type="secondary"
         autoCorrect={false}
         autoCapitalize="none"
-       />
+      />
 
-        <Input
-          placeholder="Senha"
-          type="secondary"
-          secureTextEntry
-       />
-
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+      <Button title="Button" />
     </Container>
-  )
+  );
 }
-
-
