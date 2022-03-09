221c9a2cae9c14fd92df78e1ac7110d024d7e251
henrique2m <btdavintehenrique@gmail.com>
2022-03-08 22:19:03 -0300

    :sparkles: user registration :rocket:

--- App.tsx
+++ App.tsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { useFonts, DMSans_400Regular} from '@expo-google-fonts/dm-sans';
-import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import { ThemeProvider } from "styled-components";
 
-import theme from './src/theme';
+import theme from "./src/theme";
 
-import { SignIn } from '@screens/SignIn';
-import { StatusBar } from 'expo-status-bar';
+import { SignIn } from "@screens/SignIn";
+import { StatusBar } from "expo-status-bar";
+import { AuthProvider } from "@hooks/auth";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
-    DMSerifDisplay_400Regular
+    DMSerifDisplay_400Regular,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="light" translucent backgroundColor='transparent'/>
-      <SignIn></SignIn>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+
+      <AuthProvider>
+        <SignIn></SignIn>
+      </AuthProvider>
     </ThemeProvider>
-  )
+  );
 }
-
-
