c5f53a5f46a5028f0d92a23e435cb030e84f0717
henrique2m <btdavintehenrique@gmail.com>
2022-04-05 22:09:50 -0300

    :sparkles: ImagePicker installation :rocket:

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform, TouchableOpacity } from "react-native";
-import { Container, Header, Title, DeleteLabel } from "./style";
+
+import * as ImagePicker from "expo-image-picker";
+
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+} from "./style";
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
 
 export function Product() {
+  const [image, setImage] = useState("");
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === "granted") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    }
+  }
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
@@ -17,7 +43,14 @@ export function Product() {
         </TouchableOpacity>
       </Header>
 
-      <Photo uri="" />
+      <Upload>
+        <Photo uri={image} />
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickerImage}
+        />
+      </Upload>
     </Container>
   );
 }
